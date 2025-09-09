5526a457484f1773397d9e8a0fd862440e317df8
Yefersn15 <yefersonandres225@gmail.com>
2025-09-08 23:50:52 -0500

    Configuración para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Proyecyo_Five/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
